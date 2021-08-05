145593b49f910ea27d9be324a652d62f3c05d0f3
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-08-05 17:49:05 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Hello world</h1>
     </div>
   );

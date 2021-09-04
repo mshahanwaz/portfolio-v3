24701973581f64960f631268f2f2dd2280e7f656
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-09-05 00:43:47 +0530

    Build Blogs page

--- src/components/blog/Blogs.tsx
+++ src/components/blog/Blogs.tsx
@@ -1,9 +1,24 @@
 import "./Blogs.scss";
+import Blog from "./component/Blog";
 
 function Blogs() {
   return (
     <main className="blogs container" id="blogs">
-      <h1>hello</h1>
+      <div className="blogs__wrapper">
+        <div className="blogs__media">
+          <div className="blogs__title">
+            <h2>Blogs</h2>
+          </div>
+          <div className="blogs__list">
+            <Blog />
+            <Blog />
+            <Blog />
+          </div>
+          <div className="blogs__button">
+            <button>Explore</button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ src/components/blog/component/Blog.tsx
@@ -0,0 +1,33 @@
+import "./Blog.scss";
+import W1 from "../../../assets/images/w-1.png";
+
+function Blog() {
+  return (
+    <div className="blog">
+      <div className="blog__wrapper">
+        <div className="blog__image">
+          <a href="/#asdf">
+            <img src={W1} alt="" />
+          </a>
+        </div>
+        <div className="blog__details">
+          <span>21 Sept, 2021</span>
+          <a href="/#1poi">
+            <h3 className="h-3">Title - Very Big</h3>
+          </a>
+          <p className="para">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit Ullam
+            suscipit, alias.
+          </p>
+          <div className="blog__tags">
+            <span className="tag">ReactJS</span>
+            <span className="tag">Firebase</span>
+            <span className="tag">Fake Store API</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Blog;

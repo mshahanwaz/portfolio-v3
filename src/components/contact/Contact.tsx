92b95534bbcd1a480fcf71449e59a0d386c12da0
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-09-05 02:02:52 +0530

    Build Contact page

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,7 +1,46 @@
+import Footer from "../footer/Footer";
 import "./Contact.scss";
 
 function Contact() {
-  return <main className="contact container"></main>;
+  return (
+    <main className="contact container" id="findme">
+      <div className="contact__wrapper">
+        <div className="contact__media">
+          <div className="contact__title">
+            <h2>Find Me</h2>
+          </div>
+          <div className="contact__social">
+            <a href="/#1">
+              <i className="bi bi-github" />
+            </a>
+            <a href="/#1">
+              <i className="bi bi-linkedin" />
+            </a>
+            <a href="/#1">
+              <i className="bi bi-instagram" />
+            </a>
+            <a href="/#1">
+              <i className="bi bi-twitter" />
+            </a>
+          </div>
+          <div className="contact__emailPhone">
+            <a className="link" href="mailto:mohdshahanwaz110@gmail.com">
+              <i className="bi bi-envelope" />
+              <p>mohdshahanwaz110@gmail.com</p>
+            </a>
+            <a className="link" href="tel:+918802182176">
+              <i className="bi bi-telephone" />
+              <p>+91 88021 82176</p>
+            </a>
+          </div>
+          <div className="contact__button">
+            <button>Support Me</button>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </main>
+  );
 }
 
 export default Contact;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__wrapper">
+        <p>
+          Created by <span>M. Shahanwaz</span> 💎
+        </p>
+        <p>2021 &copy; All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;

2517879f91fe0fa607c3694beda537c4b51950e6
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-09-05 14:02:34 +0530

    Build Skills page

--- src/components/skill/Skills.tsx
+++ src/components/skill/Skills.tsx
@@ -1,7 +1,51 @@
 import "./Skills.scss";
 
 function Skills() {
-  return <main className="skills container"></main>;
+  return (
+    <main className="skills container" id="skills">
+      <div className="skills__wrapper">
+        <div className="skills__media">
+          <div>
+            <h2>Skills</h2>
+          </div>
+          <div className="skills__list">
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+            <span>ReactTS</span>
+            <span>Styled-Component</span>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Skills;

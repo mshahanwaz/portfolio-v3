2377fe2cbba5184ee9edd58f15f74445b2deec49
Mohammad Shahanwaz <mohdshahanwaz55@gmail.com>
2021-09-20 18:08:45 +0530

    Build Full Blog page and styled

--- src/pages/blogs/component/FullBlog.tsx
+++ src/pages/blogs/component/FullBlog.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import db from "../../../firebase";
 import { useParams } from "react-router-dom";
 import moment from "moment";
+import { Link } from "react-router-dom";
+import "./FullBlog.scss";
 
 function FullBlog() {
   const [blog, setBlog] = useState<any>({ fetched: 0 });
@@ -21,28 +23,23 @@ function FullBlog() {
     <p>404 page</p>
   ) : blog.fetched === 1 ? (
     <div className="fullBlog">
-      <img src={blog.image} alt="" />
-      <div className="fullBlog__details">
-        <div className="fullBlog__head">
-          <h2>
-            {blog.title}
-            <span>{blog.subtitle}</span>
-          </h2>
-          <span>{moment.unix(blog.date.seconds).format("DD MMM, YYYY")}</span>
-        </div>
-        <div className="fullBlog__tags">
-          {blog.tags.map((tag, i) => (
-            <span key={i}>{tag}</span>
-          ))}
-        </div>
-        <p className="fullBlog__description">{blog.description}</p>
-        <div className="fullBlog__links">
-          <a href={blog.github} target="_blank" rel="noreferrer noopener">
-            <i className="bi bi-github" />
-          </a>
-          <a href={blog.link} target="_blank" rel="noreferrer noopener">
-            <i className="bi bi-arrow-up-right-square" />
-          </a>
+      <div className="wrapper">
+        <Link to="/blogs">
+          <i className="bi bi-arrow-left-circle" />
+        </Link>
+        <img src={blog.image} alt="" />
+        <div className="fullBlog__details">
+          <div className="fullBlog__head">
+            <h2>{blog.title}</h2>
+            <span>{moment.unix(blog.date.seconds).format("DD MMM, YYYY")}</span>
+          </div>
+          <div className="fullBlog__tags">
+            {blog.tags.map((tag, i) => (
+              <span key={i}>{tag}</span>
+            ))}
+          </div>
+          <p className="fullBlog__description">{blog.description}</p>
+          <p className="fullBlog__blog">{blog.blog}</p>
         </div>
       </div>
     </div>

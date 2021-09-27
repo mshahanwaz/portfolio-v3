6c9816793b60c726a13733ced300e35aaafed57d
Mohammad Shahanwaz <mohammad@sourcewiz.co>
2021-09-27 23:34:10 +0530

    Made Loader section

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,13 @@
+import "./Loading.scss";
+
+function Loading() {
+  return (
+    <div className="loading">
+      <div className="loading__text">
+        <span>Loading...</span>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
